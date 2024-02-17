009612e07a02e5204cb880602782184a6494fd4b
tomyprs <tomiprs25@gmail.com>
2024-02-17 16:05:04 +0700

    rosemary: Initial vendorsetup
    Signed-off-by: tomyprs <tomiprs25@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Colors
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Function to display success message
+success() {
+    echo -e "${GREEN}$1${NC}"
+}
+
+# Function to display warning message
+warning() {
+    echo -e "${YELLOW}$1${NC}"
+}
+
+# Function to display error message
+error() {
+    echo -e "${RED}$1${NC}"
+}
+
+# Cleanup
+rm -rf device/mediatek/sepolicy_vndr
+rm -rf hardware/xiaomi
+
+# Clone repository function
+cloneRepo() {
+    repo_url="$1"
+    target_dir="$2"
+    args="${@:3}"  # Capture additional arguments
+
+    if [ -d "$target_dir" ]; then
+        warning "Directory $target_dir already exists. Skipping clone."
+    else
+        echo -e "\n${GREEN}Cloning: $repo_url${NC}"
+        git clone $args "$repo_url" "$target_dir" || { error "Failed to clone $repo_url"; }
+        
+        # After cloning, set branch if applicable
+        if [[ "$args" == *"-b "* ]]; then
+            branch=$(echo "$args" | grep -oP -- "-b \K[^ ]*")
+            cd "$target_dir" || return
+            git checkout "$branch"
+            cd - || return
+        fi
+    fi
+}
+
+# Clone repositories one by one
+cloneRepo "https://github.com/hannahmontanadeving/android_device_mediatek_sepolicy_vndr" "device/mediatek/sepolicy_vndr"
+cloneRepo "https://github.com/mt6768-dev/android_hardware_xiaomi" "hardware/xiaomi"
+cloneRepo "https://github.com/AndroDesk/proprietary_vendor_xiaomi_rosemary" "vendor/xiaomi/rosemary"
+cloneRepo "https://github.com/hannahmontanadeving/android_kernel_xiaomi_mt6785" "kernel/xiaomi/mt6785" "--depth=1 --single-branch -b lineage-21"
+
+
+# Set up builder username and hostname
+export BUILD_USERNAME=Precommit
+export BUILD_HOSTNAME=$(hostname)
+
+# Misc Config variable
+export USE_CCACHE=1
+
+# Display success message
+success "Script execution completed successfully."
